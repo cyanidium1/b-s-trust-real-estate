65668da6237ce029f7df7007d2c1fb8862e1a605
AnnaFutryk <anna.futryk@gmail.com>
2025-01-20 20:28:39 +0200

    added react router dom

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import "./index.css";
 
 import { ThemeProvider } from "@material-tailwind/react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter basename="/b-s-trust-real-estate">
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
